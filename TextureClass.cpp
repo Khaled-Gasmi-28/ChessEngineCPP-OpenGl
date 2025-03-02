ddd10e146db8ac1d50dfb6854e6a0fa5aacb474f
Khaled-Gasmi-28 <khaled.gasmi.9000@gmail.com>
2025-03-02 16:30:31 +0100

    fixed Texture rendering quality

--- TextureClass.cpp
+++ TextureClass.cpp
@@ -12,8 +12,11 @@ Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, 
     glActiveTexture(slot);
     glBindTexture(texType, ID);
 
-    glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    // Set texture filtering parameters for high quality
+    glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Set texture wrapping parameters
     glTexParameteri(texType, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(texType, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
